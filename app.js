32cffe48e4b1a0027f1ad81a81b1119fb7575ca2
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-04-24 15:47:24 +0400

    65
    45412

---
+++ app.js
@@ -0,0 +1,51 @@
+// const lash = require('os');
+
+// var lash1 = lash.freemem();
+// var lash2 = lash.loadavg();
+// var lash3 = lash.hostname();
+
+// console.log(`თავისუფალია:  ${lash1}`);
+// console.log(`ლოდინი:  ${lash2}`);
+// // console.log(`დამფუძნებელი:  ${lash3}`);
+
+
+// // const lash = require('fs');
+
+// // lash.readdir('$', function(err, files) {
+// //     if (err)  console.log('lashaaaaa', err);
+// //     else console.log('shedegi', files);
+// // })
+
+// // const Event = require('events');
+// // const emitter = new Event.EventEmitter();
+
+// // emitter.on('lashajojua', function(){
+// //     console.log('okk')
+// // });
+// // emitter.emit('lashajojua');
+
+
+
+// const Event = require('events');
+// const node = require('./node');
+// const emitter = new Event.EventEmitter();
+
+// emitter.on('lashajojua', arg =>{
+//     console.log('okk', arg)
+// });
+// emitter.emit('lashajojua', {id:3,url: 'http://'});
+
+// const node =require('./node');
+// node('message');
+
+
+
+// const Event = require('events');
+// const emitter = new Event.EventEmitter();
+
+
+// emitter.on('lashajojua', arg =>{
+//     console.log('okk', arg)
+// });
+
+// emitter.emit('lashajojua', {id: 3, url: 'http://'});
---
+++ node.js
@@ -0,0 +1,24 @@
+// const Event = require('events');
+// // const { EventEmitter } = require('stream');
+// // const emitter = new Event.EventEmitter();
+
+// var url = 'http://mylogger.io/log';
+
+
+// class node extends EventEmitter{
+//     log(message){
+//         console.log(message);
+    
+//     this.emit('lashajojua', {id:3,url: 'http://'});
+    
+//     }
+// }
+// // function log(message){
+// //     console.log(message);
+
+// //     emitter.emit('lashajojua', {id:3,url: 'http://'});
+
+// // }
+
+// module.exports = node;
+
